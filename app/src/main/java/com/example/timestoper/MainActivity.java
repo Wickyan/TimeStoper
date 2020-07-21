fa027ebcc033fe254ef949ea24df9c9a0d239dfe
wickyan <yanweiqi199888@126.com>
2020-07-22 00:18:17 +0800

    数字累加

--- app/src/main/java/com/example/timestoper/MainActivity.java
+++ app/src/main/java/com/example/timestoper/MainActivity.java
@@ -2,13 +2,63 @@ package com.example.timestoper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Vibrator;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView tv;
+    private int i = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        tv = (TextView) findViewById(R.id.textView);
+        tv.setText("aaaa");
+
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                // TODO Auto-generated method stub
+                while (true) {
+                    try {
+                        tv.setText(String.valueOf((i++)));
+                        Thread.sleep(1000);
+                    } catch (Exception e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+    }
+
+    public void demo(View view) {
+        Toast.makeText(MainActivity.this, "第个6按钮被点击了",Toast.LENGTH_SHORT).show();
+
+        /*
+         震动
+         */
+        vibrateByTime(100);
+        /*
+        默认声音
+         */
+        Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        Ringtone r = RingtoneManager.getRingtone(getApplicationContext(), notification);
+        r.play();
+    }
+    public void vibrateByTime(int time)
+    {
+        Vibrator vibrator = (Vibrator)this.getSystemService(this.VIBRATOR_SERVICE);
+        vibrator.vibrate(time);
     }
 }
